74d129065aca2c948a369736dda646437ade8fb8
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-02-27 13:09:49 +0100

    string reversal all methods with tests done

--- ReverseString/revString.js
+++ ReverseString/revString.js
@@ -2,6 +2,25 @@
 // order of characters
 
 function reverse(str){
+    // solution #1
+    // return str.split('').reverse().join('');
+
+    // solution #1 a)
+    // let arr = str.split('');
+    // arr.reverse();
+    // return arr.join('');
+
+    // solution #2
+    // let reversed = '';
+    // for (let character of str) {
+    //     reversed = character + reversed;
+    // }
+    // return reversed;
+
+    // solution #3
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed;
+    }, '');
 
 }
 
