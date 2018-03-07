2e2455df61ab3ab18dafbd7906196d5ca1360d1e
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-03-07 23:49:53 +0100

    fizzbuzz function

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -1,5 +1,20 @@
-function fizzBuzz(n){
+// Write a program that console logs the numbers from 1 to n
+// For multiples of three print "fizz" and for multiples of five
+// print "buzz". For numbers which are multiples of both three and five
+// print "fizzbuzz".
 
+function fizzBuzz(n){
+    for (let i = 1; i <= n; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log('fizzbuzz');
+        } else if(i % 3 === 0){
+            console.log('fizz');
+        } else if(i % 5 === 0){
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
 }
 
 module.exports = fizzBuzz;
