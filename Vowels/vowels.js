9d7068f7062af915cf5b400a68559c11c2008c4d
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-07-13 23:33:14 +0200

    # of vowels in the string

---
+++ Vowels/vowels.js
@@ -0,0 +1,23 @@
+// write a function that returns the number of vowels
+// used in a string. Vowels are the charakters 'a', 'e',
+// 'i', 'o' and 'u'
+
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ? matches.length : 0;
+}
+
+module.exports = vowels;
+
+// iterative solution
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ["a", "e", "i", "o", "u"];
+
+//   for (let char of str.toLowerCase()) {
+//     if (checker.includes(char)) count++;
+//   }
+
+//   return count;
+// }
