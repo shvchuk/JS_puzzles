ab174779dd6dae31d3c2b8dec221f23a128d4aff
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-07-24 16:50:09 +0200

    queue with add and remove methods

--- 1_Data_Structures/Queue/queue.js
+++ 1_Data_Structures/Queue/queue.js
@@ -1,3 +1,17 @@
-class Queue {}
+// Queue data structure with methids 'add' and 'remove'.
+
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  add(record) {
+    this.data.unshift(record);
+  }
+
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;
