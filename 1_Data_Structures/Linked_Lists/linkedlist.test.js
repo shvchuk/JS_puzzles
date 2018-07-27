108b33f87caa15fd6b72d94f7c112bddc4e5bd0e
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-07-27 13:37:42 +0200

    Node class constructor & test

--- 1_Data_Structures/Linked_Lists/linkedlist.test.js
+++ 1_Data_Structures/Linked_Lists/linkedlist.test.js
@@ -10,3 +10,11 @@ test("List is a class", () => {
 test("Node is a class", () => {
   expect(typeof Node.prototype.constructor).toEqual("function");
 });
+
+describe("A Node", () => {
+  test('has properties "data" and "next"', () => {
+    const node = new Node("a", "b");
+    expect(node.data).toEqual("a");
+    expect(node.next).toEqual("b");
+  });
+});
